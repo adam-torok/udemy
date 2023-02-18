d62d59e647a3f837c8480ad3d437693c33e248ea
Török Ádám <adam.torok@xtradevs.com>
2023-02-18 19:21:47 +0100

    Adding Charts to the app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card"
 import ExpensesFilter from "../Expenses/ExpenseFilter"
 import { useState } from "react"
 import ExpensesList from "./ExpensesList"
-
+import ExpensesChart from "./ExpensesChart"
 const Expenses = (props) => {
 
     const [selectedYear, setSelectedYear] = useState('2019');
@@ -19,6 +19,7 @@ const Expenses = (props) => {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={selectedYear} onYearSelection={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
